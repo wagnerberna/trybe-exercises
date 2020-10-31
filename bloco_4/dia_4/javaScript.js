439bf07e5f64877422d67eb65c07d6a83fa14e06
wagnerberna <cabelo29007@gmail.com>
2020-10-31 04:09:54 -0300

    1º exercício

---
+++ bloco_4/dia_4/javaScript.js
@@ -0,0 +1,15 @@
+//For/In
+//mprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+console.log("01 For In");
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+console.log(`Bem Vinda: ${info.personagem}`);
+
+//02 Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+
+personagem.recorrente = "sim";
+
