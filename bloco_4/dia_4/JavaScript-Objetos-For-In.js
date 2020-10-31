ec310770a738cf318a9e1f9f9bda4b2ef9bedf95
wagnerberna <cabelo29007@gmail.com>
2020-10-31 05:34:39 -0300

    JavaScript Dia 4 ex. parte 1

---
+++ bloco_4/dia_4/JavaScript-Objetos-For-In.js
@@ -0,0 +1,34 @@
+//For/In
+//01
+//02
+console.log("01 Objetos");
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente:"Não",
+};
+//Atribui valor:
+info.recorrente = "Sim";
+console.log(`Bem Vinda: ${info.personagem}`);
+console.table(info);
+
+//03 For In 
+console.log("03 For In - chaves:");
+for(let key in info) {
+  console.log(key);
+}
+//04 For In 
+console.log("04 For In - Valores:");
+for(let key in info) {
+  console.log(info[key]);
+}
+//05 Novo objeto
+console.log("05 Novo Objeto");
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente:"Sim",
+};
+console.table(info2);
