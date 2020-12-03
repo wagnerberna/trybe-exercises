d3313c23e69dad61bd50039338cd6f6fcda60f15
wagnerberna <cabelo29007@gmail.com>
2020-12-03 07:47:56 -0300

    bloco8

---
+++ bloco_8/dia_2/8.2-exemplos-Filter-map.js
@@ -0,0 +1,39 @@
+//filter
+//--ex1 Filter - array números pares
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+// formar refatorada:
+const isEven2 = numbers.filter((number) => number % 2 === 0);
+
+console.log(isEven2); // [ 30, 22 ]
+
+//forma longa usando outra função:
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.filter(verifyEven);
+
+console.log(isEven); // [ 30, 22 ]
+
+//--ex2 Filter - Objetos pessoas menores de 18
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+  arrayOfPeople.filter((people) => (people.age < 18))
+);
+
+console.log(verifyAgeDrive(objPeople)); // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+
+//---ex3 filter - remover objeto Ricardo
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) => {
+  return listStudents.filter((student) => student !== name); // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+};
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
