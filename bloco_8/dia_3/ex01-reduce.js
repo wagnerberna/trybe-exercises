3f929cafa35628671f766c3935ee07e3657c078f
wagnerberna <cabelo29007@gmail.com>
2020-12-07 07:37:47 -0300

    req01 teste

--- bloco_8/dia_3/ex01-reduce.js
+++ bloco_8/dia_3/ex01-reduce.js
@@ -13,6 +13,6 @@ function flatten() {
   return arrays.reduce((result, current) => {
     return result.concat(current);
   }, [])
-}
+};
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
