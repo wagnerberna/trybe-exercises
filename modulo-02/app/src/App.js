d07e52f48e1cc7c9f725234b3ad4f87255999cf0
wagnerberna <cabelo29007@gmail.com>
2021-02-04 06:12:31 -0300

    bk

--- modulo-02/app/src/App.js
+++ modulo-02/app/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter } from "react-router-dom";
 
 import './App.css';
-import ContentLinks from './components/ContentLinks';
+import NavBar from './components/NavBar';
 import ContentRoute from './components/ContentRoute';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <ContentLinks />
+        <NavBar />
         <ContentRoute />
       </BrowserRouter>
     );
